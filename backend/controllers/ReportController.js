9e8872eeea3a52ab08eb20264553d95c20eebed4
sahil <sahil06butala@gmail.com>
2025-08-16 22:52:32 +0530

    csv feature

--- backend/controllers/ReportController.js
+++ backend/controllers/ReportController.js
@@ -50,4 +50,67 @@ export const exportTaskReport = catchAsync(async (req, res) => {
 });
 
 export const exportUserReport = catchAsync(async (req, res) => {
+  const id = req?.user?._id;
+  const user = await User.find().select("name email _id");
+
+  const userTask = await Task.find().populate("assignedTo", "name email _id");
+
+  const userTaskMap = {};
+  user.forEach((singleUser) => {
+    userTaskMap[singleUser] = {
+      name: singleUser?.name,
+      email: singleUser?.email,
+      taskCount: 0,
+      pendingTasks: 0,
+      inProgressTasks: 0,
+      completedTasks: 0,
+    };
+  });
+
+  userTask.forEach((task) => {
+    if (task.assignedTo) {
+      task.assignedTo.forEach((assignedUser) => {
+        if (userTaskMap[assignedUser?._id]) {
+          userTaskMap[assignedUser?._id].taskCount += 1;
+          if (task.status === "Pending") {
+            userTaskMap[assignedUser?._id].pendingTasks += 1;
+          } else if (task.status === "In Progress") {
+            userTaskMap[assignedUser?._id].inProgressTasks += 1;
+          } else if (task.status === "Completed") {
+            userTaskMap[assignedUser?._id].completedTasks += 1;
+          }
+        }
+      });
+    }
+  });
+
+  const workbook = new exceljs.Workbook();
+  const workSheet = workbook.addWorksheet("User Task Report");
+
+  workSheet.columns = [
+    { header: "User Name", key: "name", width: 25 },
+    { header: "Email", key: "email", width: 40 },
+    { header: "Total Assigned Task", key: "taskCount", width: 25 },
+    { header: "Pending Task", key: "pendingTasks", width: 25 },
+    { header: "In Progress Tasks", key: "inProgressTasks", width: 25 },
+    { header: "Completed Tasks", key: "completedTasks", width: 25 },
+  ];
+
+  Object.values(userTaskMap).forEach((user) => {
+    workSheet.addRow(user);
+  });
+
+  res.setHeader(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  );
+
+  res.setHeader(
+    "Content-Disposition",
+    'attachment ; filename="task_report.xlsx" '
+  );
+
+  return workbook.xlsx.write(res).then(() => {
+    res.end();
+  });
 });
