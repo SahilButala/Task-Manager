e625e7908b25b4974453ad8eb23b837a87194ce1
sahil <sahil06butala@gmail.com>
2025-12-05 17:29:49 +0530

    added Loader

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -9,6 +9,7 @@ import { addThousandsSeprator } from "../../helper/helper";
 import InfoCard from "../../components/Cards/InfoCard";
 
 import { IoMdCard } from "react-icons/io";
+import Loader from "../../utils/Loader";
 
 const Dashboard = () => {
   const { user } = useSelector((state) => state.auth);
@@ -20,18 +21,25 @@ const Dashboard = () => {
   const [barChartData, setbarChartData] = useState([]);
   const [pichartData, setpichartData] = useState([]);
 
+  const [isLoading, setisLoading] = useState(false);
+
   // get dashboard data api
 
   const getdashboardData = async () => {
     try {
+      setisLoading(true);
       const { data } = await axiosInstance.get(
         apiPaths.TASKS.GET_DASHBOARD_DATA
       );
 
       if (data?.sucess === true) {
         setdahboardData(data?.data);
+        setisLoading(false);
       }
-    } catch (error) {}
+    } catch (error) {
+    } finally {
+      setisLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -40,56 +48,58 @@ const Dashboard = () => {
 
   return (
     <DashboardLayout activeMenue={"Dashboard"}>
-      <div className="card my-5">
-        <div>
-          <div className="col-span-3">
-            <h2 className="text-xl md:text-xl  ">
-              {" "}
-              Good Morning.. ! {user?.name}
-            </h2>
-            <p className="text-xl md:text-[13px] text-gray-400 mt-1.5">
-              {moment().format("dddd Do MMM YYYY")}
-            </p>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="card my-5">
+          <div>
+            <div className="col-span-3">
+              <h2 className="text-xl md:text-xl  ">
+                {" "}
+                Good Morning.. ! {user?.name}
+              </h2>
+              <p className="text-xl md:text-[13px] text-gray-400 mt-1.5">
+                {moment().format("dddd Do MMM YYYY")}
+              </p>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 mt-5 md:gap-6">
+            <InfoCard
+              icon={<IoMdCard />}
+              label={"Total Tasks"}
+              value={addThousandsSeprator(
+                dahboardData?.charts?.taskDistribution?.All || 0
+              )}
+              color={"bg-blue-400"}
+            />
+            <InfoCard
+              icon={<IoMdCard />}
+              label={"Pending Tasks"}
+              value={addThousandsSeprator(
+                dahboardData?.charts?.taskDistribution?.pending || 0
+              )}
+              color={"bg-violet-400"}
+            />
+            <InfoCard
+              icon={<IoMdCard />}
+              label={"In Progress Tasks"}
+              value={addThousandsSeprator(
+                dahboardData?.charts?.taskDistribution?.InProgress || 0
+              )}
+              color={"bg-orange-400"}
+            />
+            <InfoCard
+              icon={<IoMdCard />}
+              label={"Completed Tasks"}
+              value={addThousandsSeprator(
+                dahboardData?.charts?.taskDistribution?.Completed || 0
+              )}
+              color={"bg-green-400"}
+            />
           </div>
         </div>
-
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 mt-5 md:gap-6">
-
-          <InfoCard
-             icon={<IoMdCard/>}
-             label={"Total Tasks"}
-             value={addThousandsSeprator(
-              dahboardData?.charts?.taskDistribution?.All || 0
-             )}
-             color={"bg-blue-400"}
-          />
-          <InfoCard
-             icon={<IoMdCard/>}
-             label={"Pending Tasks"}
-             value={addThousandsSeprator(
-              dahboardData?.charts?.taskDistribution?.pending || 0
-             )}
-             color={"bg-violet-400"}
-          />
-          <InfoCard
-             icon={<IoMdCard/>}
-             label={"In Progress Tasks"}
-             value={addThousandsSeprator(
-              dahboardData?.charts?.taskDistribution?.InProgress || 0
-             )}
-             color={"bg-orange-400"}
-          />
-          <InfoCard
-             icon={<IoMdCard/>}
-             label={"Completed Tasks"}
-             value={addThousandsSeprator(
-              dahboardData?.charts?.taskDistribution?.Completed || 0
-             )}
-             color={"bg-green-400"}
-          />
-
-        </div>
-      </div>
+      )}
     </DashboardLayout>
   );
 };
